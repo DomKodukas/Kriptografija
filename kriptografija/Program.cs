ebf8890ad8d0b4eb5516d19064d5c87c5a37c3e6
DomKodukas <dominykas.pugacius@stud.svako.lt>
2022-03-22 16:20:53 +0200

    Add project files.

---
+++ kriptografija/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EncryptionDecryptionUsingSymmetricKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+            
+            Console.WriteLine("Įveskite šifravimo eilutę");
+            var str = Console.ReadLine();
+            var encryptedString = AesOperation.EncryptString(key, str);
+            Console.WriteLine($"užšifruota eilutė = {encryptedString}");
+
+            var decryptedString = AesOperation.DecryptString(key, encryptedString);
+            Console.WriteLine($"iššifruota eilutė = {decryptedString}");
+
+            Console.ReadKey();
+        }
+    }
+}
